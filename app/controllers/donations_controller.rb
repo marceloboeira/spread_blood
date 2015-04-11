7ec962d21962aa69b05a6f79b7504ee483bc5a19
Marcelo Boeira <me@marceloboeira.com>
2015-04-11 19:45:37 -0300

    [Core][Donations] Donations support and Ability

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,28 @@
+class DonationsController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @donation = Donations.new
+  end
+
+  def create
+    @donation = Donations.new(donation_params)
+    @donation.user = current_user
+    if @donation.save
+      redirect_to(timeline_path, notice: "Doação criada com sucesso!")
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @donation.destroy
+    redirect_to(timeline_path, notice: "Doação removida com sucesso!")
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:location, :donated_at, :amount, :comment)
+  end
+end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :read, User
+    can :manage, Donations
 
     can :manage, :all if user.root?
   end
