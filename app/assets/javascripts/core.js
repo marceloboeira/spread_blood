f0156f642173b7bf27e4b751bd967676d2c08cab
Marcelo Boeira <me@marceloboeira.com>
2015-04-11 10:13:12 -0300

    Base core assets

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,74 @@
+//= require jquery/jquery
+//= require bootstrap/bootstrap
+//= require moment/moment
+//= require moment/locale/pt-br
+//= require medium-editor
+//= require_self
+
+$(function(){
+  /**
+   * SpreadBlood Front-end Core
+   * @type {Object}
+   */
+  var SpreadBlood = {
+    version: $("#spreadblood-version").val(),
+    locale: $("#spreadblood-locale").val(),
+    dateFormat: "ddd, DD MMM YYYY HH:mm:ss ZZ", // RFC822 Pattern
+    CSRF: $('meta[name=csrf-token]').attr('content'),
+
+    /**
+     * Booting up front-end core
+     * @return {Function}
+     */
+    init: function() {
+      this._momentInit();
+      this._mediumInit();
+      this._tooltipInit();
+      this._bootboxInit();
+    },
+
+    /**
+     * MomentJS Locale and live update settings
+     * @return {Function}
+     */
+    _momentInit: function() {
+      moment.locale(SpreadBlood.locale);
+      SpreadBlood._momentUpdate();
+      setInterval(SpreadBlood._momentUpdate, 60000);
+    },
+
+    /**
+     * MomentJS Live update on date/time inputs
+     * @return {Function}
+     */
+    _momentUpdate: function(){
+      $("date, time").each(function(i, e) {
+        var d = moment.utc($(e).data("source"), SpreadBlood.dateFormat);
+        $(e).html(d.fromNow());
+      });
+    },
+
+    /**
+     * Medium editor start
+     * @return {Function}
+     */
+    _mediumInit: function() {
+      var e = new MediumEditor('.html-editor', {
+        buttonLabels: 'fontawesome',
+        cleanPastedHTML: false,
+        forcePlainText: false
+      });
+    },
+
+    /**
+     * Bootstrap tooltip ini
+     * @return {function}
+     */
+    _tooltipInit: function() {
+      $("[title]").tooltip();
+    }
+  };
+
+  // Booting up core
+  SpreadBlood.init();
+});
