dc8fc50038bf21d7c57ddb02687fd8b4543db605
Marcelo Boeira <me@marceloboeira.com>
2015-04-11 18:05:17 -0300

    [Core] Donation limits

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,7 +2,7 @@ class Donation < ActiveRecord::Base
   belongs_to :user, validate: true
 
   validates :location, length: { maximum: 100 }, allow_blank: false
-  validates :amount,  :numericality => { :greater_than => 99, :less_than => 1600 }
+  validates :amount,  :numericality => { greater_than: 299, less_than: 599 }
   validates :donated_at, date: true, allow_blank: false
   validates :comment, length: { maximum: 400 }, allow_blank: true
 end
