beef3cc1b1f8dc30d639356f1c8dfd194e3d558c
Marcelo Boeira <me@marceloboeira.com>
2015-04-11 16:29:15 -0300

    [Core] Adding some blood_type

--- app/models/enumerations/blood_type.rb
+++ app/models/enumerations/blood_type.rb
@@ -2,7 +2,7 @@ class BloodType < EnumerateIt::Base
   associate_values :a_positive,  :a_negative,
                    :b_positive,  :b_negative,
                    :o_positive,  :o_negative,
-                   :ab_negative, :ab_negative
+                   :ab_positive, :ab_negative
 
   sort_by :translation
 end
