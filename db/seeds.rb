60d9e0e72c8c5c154f3f8b00d951cd9a244c5f82
Marcelo Boeira <me@marceloboeira.com>
2015-04-11 11:52:25 -0300

    Spreading seeds

---
+++ db/seeds.rb
@@ -0,0 +1,60 @@
+root = User.create(username: 'blood',
+                   email: 'root@spreadblood.com',
+                   password: 'dexter',
+                   name: 'Blood',
+                   birthday: Date.parse('1994-03-07'),
+                   gender: Gender::MALE,
+                   confirmed_at: DateTime.now,
+                   blood_type: BloodType::A_POSITIVE,
+                   role: UserRole::ROOT)
+
+test = User.create(username: 'test',
+                   email: 'test@spreadblood.com',
+                   password: 'blade',
+                   name: 'Test',
+                   birthday: Date.parse('1996-02-16'),
+                   gender: Gender::MALE,
+                   confirmed_at: DateTime.now,
+                   blood_type: BloodType::A_NEGATIVE,
+                   role: UserRole::REGULAR)
+
+# Remove team members seeds after the event
+marcelo = User.create(username: 'marcelo',
+                      email: 'marceloboeira@live.com',
+                      password: '102030',
+                      name: 'Marcelo',
+                      birthday: Date.parse('1994-03-07'),
+                      gender: Gender::MALE,
+                      confirmed_at: DateTime.now,
+                      blood_type: BloodType::O_POSITIVE,
+                      role: UserRole::REGULAR)
+
+marvin = User.create(username: 'marvin',
+                      email: 'marvin.medeiros52@gmail.com',
+                      password: '102030',
+                      name: 'Marvin',
+                      birthday: Date.parse('1994-06-01'),
+                      gender: Gender::MALE,
+                      confirmed_at: DateTime.now,
+                      blood_type: BloodType::A_POSITIVE,
+                      role: UserRole::REGULAR)
+
+matheus = User.create(username: 'matheus',
+                      email: 'msl7@outlook.com',
+                      password: '102030',
+                      name: 'Matheus',
+                      birthday: Date.parse('1991-09-06'),
+                      gender: Gender::MALE,
+                      confirmed_at: DateTime.now,
+                      blood_type: BloodType::O_POSITIVE,
+                      role: UserRole::REGULAR)
+
+andre = User.create(username: 'andre',
+                      email: 'cnandre.nunes@gmail.com',
+                      password: '102030',
+                      name: 'André',
+                      birthday: Date.parse('1992-06-13'),
+                      gender: Gender::MALE,
+                      confirmed_at: DateTime.now,
+                      blood_type: BloodType::A_POSITIVE,
+                      role: UserRole::REGULAR)
