8f60b07c5b5211e055e37b8a74c2c9c85788c6a9
Marvin Medeiros <marvin.medeiros52@gmail.com>
2015-04-11 14:29:17 -0300

    [Core] Implement global variables on application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
   def datetime_ago date
     content_tag :date, nil, title: date.to_s(:long), "data-source": date.to_s(:rfc822)
   end
+
+  def resource_name
+  	:user
+  end
+
+   def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
