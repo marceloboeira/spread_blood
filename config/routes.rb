8fdf4a04cd6c1fd1acd8fbc0ccd3c166838b900e
Marcelo Boeira <me@marceloboeira.com>
2015-04-12 03:45:25 -0300

    [Ranking] Routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     }
   get '/@:username', to: 'account#show', as: 'profile'
   get '/timeline', to: 'timeline#index'
-
+  get '/ranking', to: 'ranking#index'
   resources :donations
   get '/donations/:id/destroy', to: 'donations#destroy', as: 'destroy_donation'
 end
